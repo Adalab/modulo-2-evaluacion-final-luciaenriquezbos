2bf9f2abc325e17be6aca17777d6c1a375c80892
luciaenriquezbos <eboscreative@gmail.com>
2021-05-22 11:56:06 +0200

    shows in li

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 "user strict";
 
-fetch("http://api.tvmaze.com/shows")
+fetch("http://api.tvmaze.com/shows?q=+")
   .then((response) => response.json())
-  .then((data) => {
+  .then((shows) => {
     console.log("Han llegado");
-    console.log(data);
+    console.log(shows);
 
     const showList = document.querySelector(".js-ul_list");
 
-    for (const numberdata of data) {
+    for (const show of shows) {
       const newItem = document.createElement("li");
-      const newContent = document.createTextNode(data);
-      newItem.appendChild(newContent);
+      newItem.innerHTML = `Titulo: ${show.name} <img src=${show.image.medium} alt="image">`;
       showList.appendChild(newItem);
-
-      let show = data[0];
-      newItem.innerHTML = `Titulo: ${show.name} <img src="${show.image.medium}" alt="image">`;
     }
   });
