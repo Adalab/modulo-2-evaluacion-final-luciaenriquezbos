8b27dbc88f29dc021fec37cc80cb38076a5deedd
luciaenriquezbos <eboscreative@gmail.com>
2021-05-22 15:03:39 +0200

    add click button search

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 "user strict";
 
+//boton que dispar la busqueda del formulario
+
+const form = document.querySelector(".js-form");
+const submitButton = document.querySelector(".js-submit");
+const searchInput = document.querySelector(".js-search");
+const listshows = [];
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
+form.addEventListener("submit", handleSubmit);
+
+function handleSearchResult() {
+  //coger el valor del search (actual)
+  const searchText = searchInput.value;
+
+  //meterlo en la búsqueda
+  //mostrar en pantalla (render)
+}
+submitButton.addEventListener("click", handleSearchResult);
+
+//ole
+
 fetch("http://api.tvmaze.com/shows?q=+")
   .then((response) => response.json())
   .then((shows) => {
