918d13820f48bad4649cd16338dd408f685dd9b4
luciaenriquezbos <eboscreative@gmail.com>
2021-05-22 16:38:18 +0200

    add value shows

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
 "user strict";
 
-//boton que dispar la busqueda del formulario
-
 const form = document.querySelector(".js-form");
 const submitButton = document.querySelector(".js-submit");
 const searchInput = document.querySelector(".js-search");
-const listshows = [];
-
-function handleSubmit(event) {
-  event.preventDefault();
-}
-form.addEventListener("submit", handleSubmit);
-
-function handleSearchResult() {
-  //coger el valor del search (actual)
-  const searchText = searchInput.value;
-
-  //meterlo en la búsqueda
-  //mostrar en pantalla (render)
-}
-submitButton.addEventListener("click", handleSearchResult);
-
+let allShows = [];
 //ole
 
 fetch("http://api.tvmaze.com/shows?q=+")
   .then((response) => response.json())
   .then((shows) => {
-    console.log("Han llegado");
-    console.log(shows);
+    allShows = shows;
 
     const showList = document.querySelector(".js-ul_list");
 
@@ -37,3 +19,21 @@ fetch("http://api.tvmaze.com/shows?q=+")
       showList.appendChild(newItem);
     }
   });
+
+//boton que dispar la busqueda del formulario
+function handleSubmit(event) {
+  event.preventDefault();
+}
+form.addEventListener("submit", handleSubmit);
+
+function handleSearchResult() {
+  //coger el valor del search (actual)
+  const searchText = searchInput.value;
+
+  //filtrar los datos
+  filteredShows = allShows.filter((show) => show.name.includes(searchText));
+  console.log(filteredShows);
+
+  //(render)
+}
+submitButton.addEventListener("click", handleSearchResult);
